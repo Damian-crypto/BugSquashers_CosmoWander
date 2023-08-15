c710ea32f7397a72b30dac3dad78bac554a3aca2
Damian Chamel <www.damianchamel@gmail.com>
2023-08-15 23:46:10 +0530

    springboot initialized

---
+++ backend/cosmowander/src/main/java/com/bugsquashers/cosmowander/CosmowanderApplication.java
@@ -0,0 +1,13 @@
+package com.bugsquashers.cosmowander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmowanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmowanderApplication.class, args);
+	}
+
+}
---
+++ backend/cosmowander/src/test/java/com/bugsquashers/cosmowander/CosmowanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugsquashers.cosmowander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmowanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
